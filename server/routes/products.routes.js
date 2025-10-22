4cfa7d9b79ae38b0975afc74d860a438fcdf645c
Glory Michael <mikeyyyglo12@gmail.com>
2025-10-23 00:03:16 +0100

    Small edits in the package.json file

--- server/routes/products.routes.js
+++ server/routes/products.routes.js
@@ -5,6 +5,6 @@ import { isAdmin } from "../middleware/isAdmin.middleware.js"
 const router = express.Router()
 router.route("/").get(getProducts).post(isAdmin, createProduct)
 
-router.get("/search", searchProduct)
+router.get("/products/search", searchProduct)
 
 export default router
